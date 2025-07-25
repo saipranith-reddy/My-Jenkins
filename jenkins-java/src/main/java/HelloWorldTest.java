34669c5b43a4f5ebd88049c1b94033bfdc6a5cdb
root <root@ip-10-0-10-136.ap-southeast-1.compute.internal>
2025-07-25 13:51:57 +0000

    inital commit

---
+++ jenkins-java/src/main/java/HelloWorldTest.java
@@ -0,0 +1,10 @@
+mport org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+	    @Test
+	        public void testMain() {
+			        assertTrue(true);  // dummy
+						 }
+						   }
+						     
